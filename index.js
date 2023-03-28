ceb57b66bb986716477b340104f8dc3e343beb22
DeIIL <gudellcastillo@gmail.com>
2023-03-28 17:43:40 -0300

    endpoints implemented

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/order", orderRouter);
-
+app.use((error, request, response, next) => {
+  response.status(400).send({ error: error.message });
+});
 app.listen(3333, () => {
   console.log("API Started");
 });
